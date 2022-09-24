f2ce0e29839eea0fe38ad4dbf4a84592a89d7d00
Yernar Duisebay <yernarduisebay@Yernars-MacBook-Pro.local>
2022-09-24 13:05:25 +0600

    init

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { Loader } from '@googlemaps/js-api-loader';
 
+
+var data = getData()
+var ind = 0;
+
 const apiOptions = {
     "apiKey": "AIzaSyBHiUAtpqxTupKHUJTal9qfG_Z8DKvLPOQ",
     "version": "beta"
@@ -22,96 +26,118 @@ async function initMap() {
     return new google.maps.Map(mapDiv, mapOptions);
 }
 
+var selected = 0;
+const cordPoints = [
+
+];
+
 async function initWebGLOverlayView(map) {
+  
     let scene, renderer, camera, loader;
     const webGLOverlayView = new google.maps.WebGLOverlayView();
 
     webGLOverlayView.onAdd = () => {
-        scene = new THREE.Scene();
-        camera = new THREE.PerspectiveCamera();
-        const ambientLight = new THREE.AmbientLight(0xffffff, 0.75); // soft white light
-        scene.add(ambientLight);
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.25);
-        directionalLight.position.set(0.5, -1, 0.5);
-        scene.add(directionalLight);
+      scene = new THREE.Scene();
+      camera = new THREE.PerspectiveCamera();
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.75); // soft white light
+      scene.add(ambientLight);
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.25);
+      directionalLight.position.set(0.5, -1, 0.5);
+      scene.add(directionalLight);
 
-        loader = new GLTFLoader();
-        const source = 'pin.gltf';
-        loader.load(
-            source,
-            gltf => {
-                gltf.scene.scale.set(5, 5, 5);
-                gltf.scene.rotation.x = 180 * Math.PI / 180;
-                scene.add(gltf.scene);
-            }
-        );
+      loader = new GLTFLoader();
+
     }
 
 
     webGLOverlayView.onDraw = ({ gl, transformer }) => {
-        const cordPoints = [
-          {
-            lat: 33.8004048,
-            lng: -118.3890381,
-            altitude: 72.7999878
-          },
-          {
-            lat: 33.8005925,
-            lng: -118.3887513,
-            altitude: 72.5999756
-          },
-          {
-            lat: 33.800705,
-            lng: -118.3885462,
-            altitude: 72.2000122
-          },
-          {
-            lat: 33.8008574,
-            lng: -118.3881463,
-            altitude: 72.7999878
-          },
 
-        ];
+      for (let i = 0; i < cordPoints.length; i++) {
+        const matrix = transformer.fromLatLngAltitude(cordPoints[i]);
+        camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
+        let material, geometry;
 
-
-        for (let i = 0; i < cordPoints.length; i++) {
-          const matrix = transformer.fromLatLngAltitude(cordPoints[i]);
-          camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
-          webGLOverlayView.requestRedraw();
-          renderer.render(scene, camera);
-          renderer.resetState();
+        if (i == selected) {
+          geometry = new THREE.SphereGeometry( 10, 32, 32 );
+          material = new THREE.MeshBasicMaterial({color: 'red', opacity: 0.5, transparent: true});
+        } else {
+          geometry = new THREE.SphereGeometry( 5, 32, 32 );
+          material = new THREE.MeshBasicMaterial({color: 'lightblue', opacity: 0.5, transparent: true});
         }
 
+        const sphere = new THREE.Mesh( geometry, material);
+        scene.add(sphere);
+        webGLOverlayView.requestRedraw();
+        renderer.render(scene, camera);
+        renderer.resetState();
 
-        const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-        const points = [];
-        points.push( new THREE.Vector3( - 10, 0, 0 ) );
-        points.push( new THREE.Vector3( 0, 10, 0 ) );
-        points.push( new THREE.Vector3( 10, 0, 0 ) );
-        
-        const geometry = new THREE.BufferGeometry().setFromPoints( points );
-        const line = new THREE.Line( geometry, material );
-        scene.add( line );
-        renderer.render( scene, camera );
-        
+      }
+
+
     }
+
     webGLOverlayView.onContextRestored = ({ gl }) => {
-        renderer = new THREE.WebGLRenderer({
-            canvas: gl.canvas,
-            context: gl,
-            ...gl.getContextAttributes(),
-        });
+      renderer = new THREE.WebGLRenderer({
+          canvas: gl.canvas,
+          context: gl,
+          ...gl.getContextAttributes(),
+      });
 
-        renderer.autoClear = false;
+      renderer.autoClear = false;
 
-        loader.manager.onLoad = () => {
-          
-        }
+      loader.manager.onLoad = () => {
+        
+      }
     }
     webGLOverlayView.setMap(map);
+
+
+  setInterval(myFunction, 2000)
+
+  function myFunction() {
+    cordPoints.push(data[ind])
+    ind++
+    renderer.resetState();
+  }
 }
 
 (async() => {
     const map = await initMap();
     initWebGLOverlayView(map);
 })();
+
+
+
+
+///
+/// API
+///
+function getData() {
+  return [
+      {
+        lat: 33.8004048,
+        lng: -118.3890381,
+        altitude: 72.7999878
+      },
+      {
+        lat: 33.8004048,
+        lng: -118.3890381,
+        altitude: 72.7999878
+      },
+      {
+        lat: 33.8005925,
+        lng: -118.3887513,
+        altitude: 72.5999756
+      },
+      {
+        lat: 33.800705,
+        lng: -118.3885462,
+        altitude: 72.2000122
+      },
+      {
+        lat: 33.8008574,
+        lng: -118.3881463,
+        altitude: 72.7999878
+      },
+    ];
+}
