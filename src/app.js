9314d44aba6ac4e752a5d3c0e3e643fb522c9b0f
m-didar <didarmamyrovv@gmail.com>
2022-09-24 21:51:00 +0600

    slider

--- src/app.js
+++ src/app.js
@@ -2,11 +2,71 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { Loader } from '@googlemaps/js-api-loader';
 
-
+const slider = document.getElementById("slider")
+const central_btn = document.getElementById("central_btn")
+const slider_back_btn = document.getElementById("back_btn")
+const slider_forward_btn = document.getElementById("forward_btn")
 
 /// MAP DATA
-var data = getData()
-var selected = 0;
+let data = getData()
+let selected = 0;
+let isPlaying = false;
+
+slider.addEventListener("input", (e) => {
+    selected = e.target.value
+})
+slider.addEventListener("change", (e) => {
+    selected = e.target.value
+})
+
+let timer
+
+central_btn.addEventListener("click", () => {
+    if (isPlaying) {
+        clearInterval(timer)
+        central_btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="24" height="24">' +
+            '              <path fill-rule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clip-rule="evenodd" />' +
+            '            </svg>'
+        isPlaying = false
+        return;
+    }
+    isPlaying = true
+    central_btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="24" height="24">' +
+        '  <path fill-rule="evenodd" d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z" clip-rule="evenodd" />' +
+        '</svg>'
+    const val = parseInt(slider.value)
+    if (val === data.length - 1) {
+        slider.value = 0
+        selected = 0
+    }
+    timer = setInterval(() => {
+        slider.value = parseInt(slider.value) + 1;
+        selected = parseInt(slider.value)
+        if (parseInt(slider.value) === data.length - 1) {
+            clearInterval(timer)
+            central_btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="24" height="24">' +
+                '              <path fill-rule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clip-rule="evenodd" />' +
+                '            </svg>'
+            isPlaying = false
+        }
+    }, 1000)
+})
+
+slider_back_btn.addEventListener("click", () => {
+    if (isPlaying) return;
+    const val = parseInt(slider.value)
+    if (val === 0) return;
+    slider.value = val - 1;
+    selected = val - 1;
+})
+
+slider_forward_btn.addEventListener("click", () => {
+    if (isPlaying) return;
+    const val = parseInt(slider.value)
+    if (val === data.length - 1) return;
+    slider.value = val + 1;
+    selected = val + 1;
+})
 
 const apiOptions = {
     "apiKey": "AIzaSyBHiUAtpqxTupKHUJTal9qfG_Z8DKvLPOQ",
@@ -21,26 +81,24 @@ const mapOptions = {
     "mapId": "b498aa93b3c701a9"
 }
 
-var historySlider;
+// var historySlider;
 
 async function initMap() {
     const mapDiv = document.getElementById("map");
     const apiLoader = new Loader(apiOptions);
     await apiLoader.load();
 
-    const map = new google.maps.Map(mapDiv, mapOptions)
-    const sliderDiv = document.createElement("slider")
-    historySlider = createHistoryControl(map)
-    historySlider.min = 0;
-    historySlider.max = data.length-1;
+    return new google.maps.Map(mapDiv, mapOptions)
+    // const sliderDiv = document.createElement("slider")
+    // historySlider = createHistoryControl(map)
+    // historySlider.min = 0;
+    // historySlider.max = data.length-1;
 
 
-    sliderDiv.appendChild(historySlider)
-    map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(sliderDiv)
-    return map
+    // sliderDiv.appendChild(historySlider)
+    // map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(sliderDiv)
+    // return map
 }
-
-var selected = 0;
 
 
 async function initWebGLOverlayView(map) {
@@ -65,11 +123,6 @@ async function initWebGLOverlayView(map) {
     let elements = []
 
     webGLOverlayView.onDraw = ({ gl, transformer }) => {
-      historySlider.addEventListener("input", (e) => {
-        selected = e.target.value;
-        renderer.resetState();
-      })
-
       for (let i = 0; i < elements.length; i++){
         scene.remove(elements[i])
 
@@ -125,7 +178,7 @@ async function initWebGLOverlayView(map) {
       
 
       var geometry = new THREE.SphereGeometry( 10, 16, 12 );
-      geometry.applyMatrix( new THREE.Matrix4().makeScale( 1.5, 1.2, 1 ) );
+      geometry.applyMatrix4( new THREE.Matrix4().makeScale( 1.5, 1.2, 1 ) );
       const material = new THREE.LineBasicMaterial( { color: 'lightblue', opacity: 0.7, transparent: true } );
       const ellipse = new THREE.Mesh( geometry, material );
 
@@ -186,25 +239,25 @@ function getData() {
 ///
 /// SLIDER
 ///
-const createHistoryControl = (map) => {
-  const controlSlider = document.createElement("input");
-
-  controlSlider.setAttribute("type", "range")
-  controlSlider.setAttribute("class", "w-full h-2 bg-gray-200 mb-20 rounded-lg appearance-none cursor-pointer dark:bg-gray-700")
-  controlSlider.setAttribute("min", 0)
-  controlSlider.setAttribute("max", 100)
-  controlSlider.setAttribute("value", 0)
-
-  // controlSlider.style.width = "300px"
-  // controlSlider.style.height = "200px"
-  // controlSlider.style.border = "1px solid #000000"
-
-  console.log(controlSlider)
-
-  controlSlider.addEventListener("change", (e) => {
-      console.log(e.target.value)
-  })
-
-  return controlSlider
-}
+// const createHistoryControl = (map) => {
+//   const controlSlider = document.createElement("input");
+//
+//   controlSlider.setAttribute("type", "range")
+//   controlSlider.setAttribute("class", "w-full h-2 bg-gray-200 mb-20 rounded-lg appearance-none cursor-pointer dark:bg-gray-700")
+//   controlSlider.setAttribute("min", 0)
+//   controlSlider.setAttribute("max", 100)
+//   controlSlider.setAttribute("value", 0)
+//
+//   // controlSlider.style.width = "300px"
+//   // controlSlider.style.height = "200px"
+//   // controlSlider.style.border = "1px solid #000000"
+//
+//   console.log(controlSlider)
+//
+//   controlSlider.addEventListener("change", (e) => {
+//       console.log(e.target.value)
+//   })
+//
+//   return controlSlider
+// }
 
